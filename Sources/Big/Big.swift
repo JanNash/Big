6c5b8e5a841b6675f3c2bc6585cf910de935c49a
Jan Nash <jnash@jnash.de>
2020-11-28 14:52:07 +0100

    Start implementing Number

--- Sources/Big/Big.swift
+++ Sources/Big/Big.swift
@@ -5,4 +5,27 @@
 //  Created by Jan Nash on 28.11.20.
 //
 
-import Foundation
+class Number {
+    struct Digit: Hashable {
+        struct Value {
+            private(set) var representation: String
+            private(set) var offset: UInt
+        }
+        
+        class ValueSet {
+            init(_ representations: [String]) {
+                var offset: UInt = 0
+                values = representations.reduce(into: (values: [], dupeControlCache: [String: Any]()), {
+                    guard $0.dupeControlCache[$1] == nil else { fatalError() }
+                    $0.dupeControlCache[$1] = true
+                    $0.values.append(Value(representation: $1, offset: offset))
+                    let (newOffset, overflow) = offset.addingReportingOverflow(1)
+                    guard !overflow else { fatalError() }
+                    offset = newOffset
+                }).values
+            }
+            
+            let values: [Value]
+        }
+    }
+}
